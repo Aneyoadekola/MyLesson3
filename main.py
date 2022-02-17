7097b9a9b8e963369a8b3ff07293ef4073e5e5fc
Aneyoadekola <99821407+Aneyoadekola@users.noreply.github.com>
2022-02-17 14:06:08 +0100

    Upload of Main for MyLesson3 v1

---
+++ main.py
@@ -0,0 +1,24 @@
+# Lesson 3: Multiple Assignment
+# Multiple Assignment: allows us assign multiple variables at the same time in a single line of code
+name = "Daniel"
+age = 3
+smart = True
+
+print(name)
+print(age)
+print(smart)
+
+surname, davidAge, intelligent = 'Aneyo', 5 , True
+
+print(surname)
+print(davidAge)
+print(intelligent)
+
+spade = square = circle = kite = 5
+
+print(spade)
+print(square)
+print(circle)
+print(kite)
+
+# 20:38 (time to begin another session)
